189742d3fc4c8b5282a9b0481c90983f77cb2f64
SinisterRectus <SinisterRectus@users.noreply.github.com>
2016-06-17 11:00:56 -0400

    Total rewrite

--- server/sTerrainMap.lua
+++ server/sTerrainMap.lua
@@ -1,15 +1,11 @@
 class 'TerrainMap'
 
 function TerrainMap:__init()
-
-	Network:Subscribe("TeleportToCell", self, self.TeleportToCell)
-	
+	Network:Subscribe('TeleportToPosition', self, self.OnTeleportToPosition)
 end
 
-function TerrainMap:TeleportToCell(args, sender)
-
-	sender:SetPosition(args.position + Vector3(0, 20, 0))
-
+function TerrainMap:OnTeleportToPosition(args, sender)
+	sender:SetPosition(args.position)
 end
 
 TerrainMap = TerrainMap()
